b895992a32d61a82192ba4705691e8ec8548c4e6
woongbin chae <jerpi053@gmail.com>
2020-09-18 22:17:36 +0900

    binary Tree

---
+++ inflearn_2/inflearn_2/Board.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inflearn_2
+{
+   
+    class Board
+    {
+        const char CIRCLE = '\u25cf';
+        public TileType[,] _tile;
+        public int _size;
+
+        public enum TileType
+        {
+            Empty,
+            Wall,
+        }
+
+        public void Initialize(int size)
+        {
+            if (size % 2 == 0) return; // 미로는 짝수
+
+            _tile = new TileType[size, size];
+            _size = size;
+
+            //Mazes for Programmers
+
+            GenerateByBinaryTree();
+        }
+
+        void GenerateByBinaryTree()
+        {
+            // 벽 생성
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    //if(x==0 || x == _size -1 || y ==0 || y == _size -1)
+                    if (x % 2 == 0 || y % 2 == 0)
+                    {
+                        _tile[y, x] = TileType.Wall;
+                    }
+                    else
+                    {
+                        _tile[y, x] = TileType.Empty;
+                    }
+                }
+            }
+
+            // binary tree 알고리즘
+            // 길뚫기
+            Random rand = new Random();
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    //if(x==0 || x == _size -1 || y ==0 || y == _size -1)
+                    if (x % 2 == 0 || y % 2 == 0)
+                    {
+                        continue;
+                    }
+
+
+                    if (y == _size - 2 && x == _size - 2)
+                    {
+                        continue;
+                    }
+
+                    // 외곽벽 
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+                    // 외곽벽 
+                    if (x == _size - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    // 랜덤
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                    }
+                    else
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                    }
+                }
+            }
+        }
+
+        ConsoleColor GetTileColor(TileType type)
+        {
+            switch (type)
+            {
+                case TileType.Empty: return ConsoleColor.Green;
+                case TileType.Wall: return ConsoleColor.Red;
+                default: return ConsoleColor.Green;
+            }
+        }
+
+        public void Render()
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+            for(int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+
+                    Console.ForegroundColor = GetTileColor(_tile[y,x]);
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+        
---
+++ inflearn_2/inflearn_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace inflearn_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            board.Initialize(25);
+
+            Console.CursorVisible = false;
+
+
+            const int WAIT_TICK = 1000 / 30;
+
+            int lastTick = 0;
+
+            while (true)
+            {
+                Console.SetCursorPosition(0,0);
+                board.Render();
+            }
+        }
+    }
+}
